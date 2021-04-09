0e6623df1a199c656854babd82227343e6193d43
sebastian0912 <62730462+sebastian0912@users.noreply.github.com>
2021-04-09 09:10:38 -0500

    Add files via upload

---
+++ src/components/footer.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">    
+    <center> 
+       <img id="img4" src="../assets/Acm.jpg"  >
+    <hr>    
+    <br>
+      <img id="img3" src="../assets/git.png">
+      <img id="img3" src="../assets/face.png">
+      <img id="img3" src="../assets/insta.png">
+    </center>    
+    <br>
+    <p>
+      Made with love by ACM Javeriana 2021
+    </p>
+    <br>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+p{  
+  text-align: center;
+  font-size: 15px;
+}
+#img4{
+  width: 120px;
+  height: 120px; 
+}
+#img3{  
+  margin-right: 25px;
+  width: 25px;
+  height: 25px;  
+}
+
+</style>
