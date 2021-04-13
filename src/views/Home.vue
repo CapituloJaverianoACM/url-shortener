6e78517dcd72321c93ade6919db4deabbca4d743
Juliana <rojasnpaula@javeriana.edu.co>
2021-04-13 18:02:42 -0500

    add header with white brackground-color

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,9 @@
 <template>
-<<<<<<< HEAD
   <v-main>
     <Header/>
     <Home/>
      <!-- <Footer/> -->
   </v-main>
-=======
-  <div class="home">
-    <Header />
-    <!-- <Footer/> -->
-  </div>
->>>>>>> e624f7735802cfdb44cfd51f92383229469d594d
 </template>
 
 <script>
@@ -21,12 +14,8 @@ import Home from "../components/home/Home.vue";
 export default {
   components: {
     Header,
-<<<<<<< HEAD
     Home,
    // Footer,
-=======
-    // Footer,
->>>>>>> e624f7735802cfdb44cfd51f92383229469d594d
   },
 };
 </script>
