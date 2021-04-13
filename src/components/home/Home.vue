da9d17da6c8d7b189ff40cd28c0171028f81bc19
monotera <nelson.a.mosquera@gmail.com>
2021-04-13 17:31:14 -0500

    Tests v-btn

--- src/components/home/Home.vue
+++ src/components/home/Home.vue
@@ -1,43 +1,43 @@
 <template>
-    <div class="home">
-        <div class="text">
-            <h1>Welcome to ACM's URL Shortener</h1>
-             <p>A URL shortener built with powerful tools to help you grow and protect your brand.</p>
-             <v-btn class = "button">Try it!</v-btn>
-        </div>
-         <img src="../../assets/imgs/home.svg"/>
-         
+  <div class="home">
+    <div class="text">
+      <h1>Welcome to ACM's URL Shortener</h1>
+      <p>
+        A URL shortener built with powerful tools to help you grow and protect
+        your brand.
+      </p>
+      <v-btn color="#0D91CF" dark class="button">Try it!</v-btn>
     </div>
+    <img src="../../assets/imgs/home.svg" />
+  </div>
 </template>
 
-<script>
-</script>
+<script></script>
 
-<style scoped lang = "scss">
-
-img{
-    float: right;
-    margin-right: 90px;
+<style scoped lang="scss">
+img {
+  float: right;
+  margin-right: 90px;
 }
-.home{
-    margin-top: 100px;
-    display: flex;
-    justify-content: space-around;
-    align-items: center;
+.home {
+  margin-top: 100px;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
 }
-.text{
-    display: absolute;
-    margin-left: 90px;
+.text {
+  display: absolute;
+  margin-left: 90px;
 }
-h1{
-    font-family: $comfortaaBold-font;
-    font-size: 45px;
+h1 {
+  font-family: $comfortaaBold-font;
+  font-size: 45px;
 }
-p{
-    font-family: $montserratMedium-font;
+p {
+  font-family: $montserratMedium-font;
 }
 
-.button{
-    font-family: $montserratMedium-font !important;
+.button {
+  font-family: $montserratBold-font;
 }
 </style>
