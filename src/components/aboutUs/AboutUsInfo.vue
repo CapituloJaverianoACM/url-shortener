c720cef4d52814c873bf0c57660ce00cb2a29747
monotera <nelson.a.mosquera@gmail.com>
2021-05-10 17:14:23 -0500

    Resolves minor request from aboutUs page

--- src/components/aboutUs/AboutUsInfo.vue
+++ src/components/aboutUs/AboutUsInfo.vue
@@ -4,46 +4,39 @@
       <div class="info-section">
         <h2>What is ACM?</h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged. It was popularised in the 1960s with
-          the release of Letraset sheets containing Lorem Ipsum passages, and
-          more recently with desktop publishing software like Aldus PageMaker
-          including versions of Lorem Ipsum.
+          The Association for Computing Machinery (ACM) is an international
+          knowledge society for computing. It was founded in 1947, and is the
+          largest scientific and educational computing society in the world. It
+          is a non-profit professional membership group. It has over 100,000
+          members as of 2011. Its headquarters are in New York City. His motto
+          is "Advance computing as a science and profession."
         </p>
       </div>
       <img src="../../assets/img/aboutUs.svg" height="417" width="500" />
     </div>
     <div class="section reverse">
-      <img src="../../assets/img/mision.svg" height="367" width="500" />
+      <img src="../../assets/img/mision.svg" height="337" width="500" />
       <div class="info-section">
-        <h2>Mision</h2>
+        <h2>What is the chapter?</h2>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged
+          We are a student group from the Engineering Faculty of the Pontificia
+          Universidad Javeriana, with a history of 10 years. We are formalized
+          in the Vice-rectory of the University Environment, from the Center for
+          the Promotion of Identity and Construction of the Community,
+          particularly in the University Participation Program and directly
+          accompanied by the Faculty of Engineering. We carry out activities
+          that mainly involve students from the Faculty of Engineering and
+          students from schools who are interested in the Systems Engineering
+          career, we also participate in the National Programming Marathon that
+          takes place every year, however, we open the doors for everyone who is
+          interested to support us and contribute to the objective of ACM.
+          <span
+            ><a class="learn-more" href="http://acm.capitulojaveriano.com/"
+              >Learn more...</a
+            ></span
+          >
         </p>
       </div>
-    </div>
-    <div class="section">
-      <div class="info-section">
-        <h2>Vision</h2>
-        <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting
-          industry. Lorem Ipsum has been the industry's standard dummy text ever
-          since the 1500s, when an unknown printer took a galley of type and
-          scrambled it to make a type specimen book. It has survived not only
-          five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged
-        </p>
-      </div>
-      <img src="../../assets/img/vision.svg" height="337" width="500" />
     </div>
   </section>
 </template>
@@ -86,6 +79,10 @@ export default {};
   }
   p {
     margin-bottom: 0;
+  }
+  .learn-more {
+    color: $acm-color;
+    text-decoration: none;
   }
 }
 .reverse {
